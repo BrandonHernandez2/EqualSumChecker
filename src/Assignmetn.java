a3cf5f0cc63ebfd756a8c4240d7be9af3a9b5f44
hernabra000 <hernabra000@warren.k12.in.us>
2024-08-27 12:46:35 -0400

    finished method

--- src/Assignmetn.java
+++ src/Assignmetn.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Assignmetn {
+public class Assignmetn {
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1));
+        System.out.println(hasEqualSum(1, 1, 2));
+        System.out.println(hasEqualSum(1, -1, 0));
+    }
+
+    private static boolean hasEqualSum(int num1, int num2, int num3) {
+        //find sum of num1 and num2 store it as variable
+
+        int sum = num1 + num2;
+
+        //return whether that same is equal to num3;
+
+        if (sum == num3){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
